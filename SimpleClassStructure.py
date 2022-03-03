66871ab31b322fbdd835173c035c3d7edf65fe06
Musar98 <40075894+Musar98@users.noreply.github.com>
2022-03-03 19:06:12 +0100

    Update SimpleClassStructure.py

--- SimpleClassStructure.py
+++ SimpleClassStructure.py
@@ -3,11 +3,11 @@ import tkinter
 class RootConfig:
     def __init__(self,root):
         self.root = root
-        self.title = self.root.title("Tkinter-Class")
-        self.config = self.root.configure(bg="black", )
-        self.x = int(self.root.winfo_screenwidth() / 2 - (self.root.winfo_reqwidth() / 2))
-        self.y = int(self.root.winfo_screenheight() / 3 - (self.root.winfo_reqheight() / 2))
-        self.geometry = self.root.geometry(f"+{self.x}+{self.y}")
+        self.title = root.title("Tkinter-Class")
+        self.config = root.configure(bg="black", )
+        self.x = int(root.winfo_screenwidth() / 2 - (root.winfo_reqwidth() / 2))
+        self.y = int(root.winfo_screenheight() / 3 - (root.winfo_reqheight() / 2))
+        self.geometry = root.geometry(f"+{self.x}+{self.y}")
 
 if __name__ == "__main__":
     root = tkinter.Tk()
